bf015439e7f89b2444c2bf2e5e0e88090d89ddc4
meimei <15210237516@163.com>
2018-11-24 16:52:22 +0800

    01commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+name = input('请输入要备份的文件名:')
+f = open(name,'r')
+content = f.read()
+f.close()
+print(name.split('.'))
+name_list = name.split('.')
+new_name = name_list[0] + '-copy.'+ name_list[1]
+f = open(new_name,'w')
+content = f.
